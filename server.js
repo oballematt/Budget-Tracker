7695e404c021554f6968330e27a3d1bdb2d96c2a
Matthew Oballe <oballematt@gmail.com>
2020-11-17 15:42:51 -0600

    making initial commit. Installing all necessary dependencies

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
+
+db.newdatabase.insert({
+  "name2": "this is name2",
+  "title": ["item 1", "item2", "item3"],
+  "country": "this is country"
+})
